b663714ad1d92e552629debe99b787ff86985e8e
guillermodotn <guillerm0.n@outlook.es>
2025-03-06 00:33:17 +0100

    Partition drive.

--- isoigniter/win_iso_writer.py
+++ isoigniter/win_iso_writer.py
@@ -1,7 +1,7 @@
 from PySide6.QtCore import QThread, Signal
 import logging
 import subprocess
-
+import re
 
 class WinWriterThread(QThread):
     progress = Signal(int)
@@ -20,26 +20,46 @@ class WinWriterThread(QThread):
             self.finished.emit(False, str(e))  # Emit error message
 
     def create_partition_table(self, device):
-        """Creates a GPT partition table and a FAT32 partition on the given device."""
+        """Creates a GPT partition table and partions on the device."""
 
         try:
+            # Get the size of the device
+            result = subprocess.run(["lsblk", "-b", "-n", "-d", "-o", "SIZE", device], check=True, stdout=subprocess.PIPE, text=True)
+            logging.info(f"Device size: {result.stdout.strip()} bytes")
+            win_data_partition_size = int(result.stdout.strip()) // 1024**2 - 10  # 10MB less than the total size
+            logging.info(f"Win data partition size: {win_data_partition_size} MiB")
+
             # Create a new GPT partition table
             subprocess.run(["parted", "-s", device, "mklabel", "gpt"], check=True)
             logging.info(f"Created GPT partition table on {device}")
 
             # Create a new primary partition (FAT32, using 100% of disk)
             subprocess.run(
-                ["parted", "-s", device, "mkpart", "primary", "fat32", "1MiB", "100%"],
+                ["parted", "-s", device, "mkpart", "primary", "fat32", f"{win_data_partition_size}MiB", "100%"],
                 check=True,
             )
-            logging.info(f"Created primary partition on {device}")
+            logging.info(f"Created primary partition (boot) on {device}")
 
-            # Format the partition as FAT32 (assuming it's /dev/sdb1)
+            # Create a new primary partition (FAT32, using 100% of disk)
+            subprocess.run(
+                ["parted", "-s", device, "mkpart", "primary", "ntfs", "0%", "100%"],
+                check=True,
+            )
+            logging.info(f"Created primary partition (Win data) on {device}")
+
+            # Format the partition as NTFS (assuming it's /dev/sda1)
             partition = f"{device}1"
+            subprocess.run(
+                ["mkfs.ntfs", "-f", partition], check=True, stdout=subprocess.DEVNULL
+            )
+            logging.info(f"Partition table created and formatted on {partition}")
+
+            # Format the partition as FAT32 (assuming it's /dev/sda2)
+            partition = f"{device}2"
             subprocess.run(
                 ["mkfs.vfat", "-F32", partition], check=True, stdout=subprocess.DEVNULL
             )
-            logging.info(f"Partition table created and formatted on {device}")
+            logging.info(f"Partition table created and formatted on {partition}")
 
         except subprocess.CalledProcessError as e:
             logging.error(f"Error: {e}")
